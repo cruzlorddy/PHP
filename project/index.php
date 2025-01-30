db9b6d3b795244e9b394c7a6856bd3ea0b4dd0de
Cruz lord tek <destinyfrosh952@gmail.com>
2025-01-30 16:32:56 +0100

    First PHP class

---
+++ project/index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $W = "welcome to php";
+        echo $W;
+    ?>
+    <h1>Welcome to PHP tutorial</h1>
+</body>
+</html>
